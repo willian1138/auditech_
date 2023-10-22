2df802edf18692ad5215c17cd68c56a0395f0a3c
william <psilva546@hotmail.com>
2023-10-22 16:40:11 -0300

    dia 22 10 2023

--- src/main/java/fbw/auditech/service/PacienteService.java
+++ src/main/java/fbw/auditech/service/PacienteService.java
@@ -1,14 +1,10 @@
 package fbw.auditech.service;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-//import java.util.UUID;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
@@ -20,89 +16,75 @@ import fbw.auditech.model.Paciente;
 @Service
 public class PacienteService implements IPacienteService {
 
-	Logger logger = LogManager.getLogger(this.getClass());
+    Logger logger = LogManager.getLogger(this.getClass());
 
-	@Autowired
-	IPacienteRepository repositoryP;
-	@Autowired
-	FotoService fotoService;
+    @Autowired
+    IPacienteRepository repositoryP;
 
-	@Override
-	public List<Registro> consultaRegistroPacientes() {
+    @Autowired
+    FotoService fotoService;
 
-		Registro c = null;
-		List < Registro > lista = new ArrayList < Registro > ();
-		List < Paciente > listaP = repositoryP.findAll();
-		for (Paciente p : listaP) {
-			c = new Registro(p.getId(), p.getNome(), p.getCpf());
-			lista.add(c);
-		}
-		return lista;
-	}
-			
-		
-		
-	
-	@Override
-	public List<Paciente> consultaPorNome() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'consultaPorDescricao'");
-	}
-	@Override
-	public Optional<Paciente> cadastrar(Paciente paciente) {
-		// TODO Auto-generated method stub
-		logger.info(">>>servico cadastrar paciente iniciado");
-		return Optional.ofNullable(repositoryP.save(paciente));
-		//throw new UnsupportedOperationException("Unimplemented method 'cadastrar'");
-	}
-	@Override
-	public Optional<Paciente> consultarPorId(String id) throws ResourceNotFoundException {
+    @Override
+    public List<Registro> consultaRegistroPacientes() {
+        Registro c = null;
+        List<Registro> lista = new ArrayList<Registro>();
+        List<Paciente> listaP = repositoryP.findAll();
+        for (Paciente p : listaP) {
+            c = new Registro(p.getId(), p.getNome(), p.getCpf());
+            lista.add(c);
+        }
+        return lista;
+    }
 
-		logger.info(">>>servico cadastrar paciente iniciado");
-		Optional<Paciente> paciente = repositoryP.findById(Long.parseLong(id));
-		if(paciente.isPresent()){
-			return paciente;
-		}
-		else{}
-		// TODO Auto-generated method stub
-		throw new ResourceNotFoundException("Paciente não encontrado para o id '" + id + "'");
-	}
+    @Override
+    public List<Paciente> consultaPorNome() {
+        throw new UnsupportedOperationException("Método 'consultaPorNome' não implementado");
+    }
 
+    @Override
+    public Optional<Paciente> cadastrar(Paciente paciente) {
+        logger.info("Serviço de cadastro de paciente iniciado");
+        return Optional.ofNullable(repositoryP.save(paciente));
+    }
 
-	@Override
-	public Optional<Paciente> atualizar(Long id, Paciente pacienteAtualizado) {
-		Optional<Paciente> pacienteExistente = repositoryP.findById(id);
-		
-		if(pacienteExistente.isPresent()){
-			Paciente paciente = pacienteExistente.get();
+    @Override
+    public Optional<Paciente> consultarPorId(String id) throws ResourceNotFoundException {
+        logger.info("Serviço de consulta de paciente por ID iniciado");
+        Optional<Paciente> paciente = repositoryP.findById(Long.parseLong(id));
+        if (paciente.isPresent()) {
+            return paciente;
+        } else {
+            throw new ResourceNotFoundException("Paciente não encontrado para o ID '" + id + "'");
+        }
+    }
 
-			paciente.setNome(pacienteAtualizado.getNome());
-			paciente.setCpf(pacienteAtualizado.getCpf());
-			paciente.setDataDeNascimento(pacienteAtualizado.getDataDeNascimento());	
-			paciente.setNumeroDeTelefone(pacienteAtualizado.getNumeroDeTelefone();
-			paciente.setEndereco(pacienteAtualizado.getEndereco());
-			paciente.setEmail(pacienteAtualizado.getEmail());
-			paciente.setSenha(pacienteAtualizado.getSenha());
-			return Optional.ofNullable(repositoryP.save(paciente));
-		} else{
-			return Optional.empty();
-		}
-		
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void excluir(Long id) throws ResourceNotFoundException {
-		Optional<Paciente> pacienteExistente = repositoryP.findById(id);
-		if(pacienteExistente.isPresent()){
-			repositoryP.deleteById(id);
-		}else {
-			throw new ResourceNotFoundException();
+    @Override
+    public Optional<Paciente> atualizar(Long id, Paciente pacienteAtualizado) {
+        Optional<Paciente> pacienteExistente = repositoryP.findById(id);
 
-		}
+        if (pacienteExistente.isPresent()) {
+            Paciente paciente = pacienteExistente.get();
 
-		// TODO Auto-generated method stub
-		//throw new UnsupportedOperationException("Unimplemented method 'excluir'");
-	}
-    
+            paciente.setNome(pacienteAtualizado.getNome());
+            paciente.setCpf(pacienteAtualizado.getCpf());
+            paciente.setDataDeNascimento(pacienteAtualizado.getDataDeNascimento());
+            paciente.setNumeroDeTelefone(pacienteAtualizado.getNumeroDeTelefone());
+            paciente.setEndereco(pacienteAtualizado.getEndereco());
+            paciente.setEmail(pacienteAtualizado.getEmail());
+            paciente.setSenha(pacienteAtualizado.getSenha());
+            return Optional.ofNullable(repositoryP.save(paciente));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void excluir(Long id) throws ResourceNotFoundException {
+        Optional<Paciente> pacienteExistente = repositoryP.findById(id);
+        if (pacienteExistente.isPresent()) {
+            repositoryP.deleteById(id);
+        } else {
+            throw new ResourceNotFoundException();
+        }
+    }
 }
